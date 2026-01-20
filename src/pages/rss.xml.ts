94f69c6fdcb486eceeb026e9109cb73eab79b026
Claudiu Ivan <contact@claudiu-ivan.com>
2026-01-20 22:24:17 +0100

    feat: add llms.txt, RSS author, fix Twitter card image

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,30 +1,37 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { HOME } from "@consts";
+import { HOME, AUTHOR, SITE } from "@consts";
 
 type Context = {
-  site: string
-}
+  site: string;
+};
 
 export async function GET(context: Context) {
-  const writing = (await getCollection("writing"))
-  .filter(article => !article.data.draft);
+  const writing = (await getCollection("writing")).filter(
+    (article) => !article.data.draft
+  );
 
-  const projects = (await getCollection("projects"))
-    .filter(project => !project.data.draft);
+  const projects = (await getCollection("projects")).filter(
+    (project) => !project.data.draft
+  );
 
-  const items = [...writing, ...projects]
-    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+  const items = [...writing, ...projects].sort(
+    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
+  );
 
   return rss({
     title: HOME.TITLE,
     description: HOME.DESCRIPTION,
     site: context.site,
+    customData: `<language>en-us</language>
+<managingEditor>${SITE.EMAIL} (${AUTHOR.NAME})</managingEditor>
+<webMaster>${SITE.EMAIL} (${AUTHOR.NAME})</webMaster>`,
     items: items.map((item) => ({
       title: item.data.title,
       description: item.data.description,
       pubDate: item.data.date,
       link: `/${item.collection}/${item.slug}/`,
+      author: `${SITE.EMAIL} (${AUTHOR.NAME})`,
     })),
   });
 }
